580d16f06a821d8492323f0141a86daceb341a06
Alvin Kiveu <67068458+alvin-kiveu@users.noreply.github.com>
2024-01-21 21:41:56 +0300

    adding new data

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Mpesa Daraja Api Php Sdk
+
+This is a php sdk for the new Mpesa Daraja Api. It is still in development and will be updated as the api changes.
+
+## Installation
+
+You can install the package via composer:
+
+```bash
+composer require ums/mpesa
+```
+
+## ENVIRONMENT VARIABLES
+
+```bash
+MPESA_ENV= production # or sandbox >
+MPESA_CONSUMER_KEY= add your consumer key
+MPESA_CONSUMER_SECRET= add your consumer secret
+MPESA_PASSKEY= add your passkey
+MPESA_SHORTCODE= add your shortcode
+MPESA_CALLBACK_URL= add your callback url
+```
--- src/MpesaSdk.php
+++ src/MpesaSdk.php
@@ -4,8 +4,25 @@ namespace MpesaSdk;
 
 class MpesaSdk
 {
-    public function __construct()
-    {
-        echo 'Hello World!';
-    }
+  public function generateToken($consumer_key, $consumer_secret)
+  {
+    $credentials = base64_encode($consumer_key . ":" . $consumer_secret);
+    $url = 'https://sandbox.intasend.com/api/v1/oauth/token/';
+    $payload = [
+      "grant_type" => "client_credentials"
+    ];
+    $payload = json_encode($payload);
+    $headers = [
+      'Authorization: Basic ' . $credentials,
+      'Content-Type: application/json'
+    ];
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $payload);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($curl);
+    curl_close($curl);
+    $response = json_decode($response);
+    return $response->access_token;
+  }
 }
